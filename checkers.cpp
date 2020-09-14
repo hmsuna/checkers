33805b145cff3221a8d724cff96099a654806d17
hmsuna <70170393+hmsuna@users.noreply.github.com>
2020-09-13 22:08:27 -0500

    added checkers.cpp file

---
+++ checkers.cpp
@@ -0,0 +1,30 @@
+/*
+	Haley Montgomery
+	C++ 2020
+	Due: Sep 14
+	Lab 2.2 Exploring Output
+	Lab description This lab is designed to get you familiar out to the command promt using C++. Complete the following problems
+*/
+
+#include <iostream>		
+
+int main()
+{
+    // define two row patterns
+    const std::string ROW_A= "     *           *           *           *";
+    const std::string ROW_B= "*          *           *           *";
+    
+    // print rows of checkers board
+    std::cout << ROW_A << std::endl;
+    std::cout << ROW_B << std::endl;
+    std::cout << ROW_A << std::endl;
+    std::cout << ROW_B << std::endl;
+    std::cout << ROW_A << std::endl;
+    std::cout << ROW_B << std::endl;
+    std::cout << ROW_A << std::endl;
+    std::cout << ROW_B << std::endl;
+
+	// Complete the function by returning the integer 0 to main. This tells is a way
+	// of saying that the main function completed with no errors.
+	return 0;
+}
